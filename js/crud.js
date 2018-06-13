2cee2da84f2e1b0209c641f2c3379320afd2979e
dwarakcheyyur <dwarak@cloudreign.in>
2018-06-13 14:55:42 +0530

    added js file

--- js/crud.js
+++ js/crud.js
@@ -1,309 +1,83 @@
-jQuery( document ).ready(function( $ ) {
-        // variable to hold request
-        var request;
-        // bind to the submit event of our form
-           $("#formCreate").submit(function(event){
-       // abort any pending request
-       if (request) {
-           request.abort();
-       }
-       // setup some local variables
-       var $form = $(this);
-       // let's select and cache all the fields
-       var $inputs = $form.find("input, select, button, textarea");
-       // serialize the data in the form
-       var serializedData = $form.serialize();
- 
-       // let's disable the inputs for the duration of the ajax request
-       // Note: we disable elements AFTER the form data has been serialized.
-       // Disabled form elements will not be serialized.
-       $inputs.prop("disabled", true);
-       $('#result').text('Sending data...');
- 
-       // fire off the request to /form.php
-       request = $.ajax({
-           url: "https://script.google.com/macros/s/AKfycbxK6OlTh5D_HSAkgoQl9CgnmRmXsGbzpqzTqiC0isiFhJl0yeKN/exec",
-           type: "post",
-           data: serializedData
-       });
- 
-       // callback handler that will be called on success
-       request.done(function (response, textStatus, jqXHR){
-           // log a message to the console
-           $('#result').html('<a href="https://docs.google.com/spreadsheets/d/1KZ3jW3X7xtCzTP_iGhdsIUKjgHjzKqkvdXNn2z7izlA/edit?usp=sharing" target="_blank">Success - see Google Sheet</a>');
-     console.log("Created.");
-     //console.log(response);
-     //console.log(textStatus);
-     //console.log(jqXHR);
-     resetForm();
-     $("#btnRetrieve").click();
-       });
- 
-       // callback handler that will be called on failure
-       request.fail(function (jqXHR, textStatus, errorThrown){
-           // log the error to the console
-           console.error(
-               "The following error occured: "+
-               textStatus, errorThrown
-           );
-       });
- 
-       // callback handler that will be called regardless
-       // if the request failed or succeeded
-       request.always(function () {
-           // reenable the inputs
-           $inputs.prop("disabled", false);
-       });
- 
-       // prevent default posting of form
-       event.preventDefault();
-   });
+// Make an AJAX call to Google Script
+var script_url = "https://script.google.com/macros/s/AKfycbxK6OlTh5D_HSAkgoQl9CgnmRmXsGbzpqzTqiC0isiFhJl0yeKN/exec";
+function insert_value() {		
+	$("#re").css("visibility","hidden");
+	document.getElementById("loader").style.visibility = "visible";
+	$('#mySpinner').addClass('spinner');
+	var id1= $("#id").val();
+	var name= $("#name").val();		
+	var url = script_url+"?callback=ctrlq&name="+name+"&id="+id1+"&action=insert";
+	var request = jQuery.ajax({
+		crossDomain: true,
+		url: url ,
+		method: "GET",
+		dataType: "json"
+	});
+}
 
+function update_value(){
+	$("#re").css("visibility","hidden");
+	document.getElementById("loader").style.visibility = "visible";
+	var id1=	$("#id").val();
+	var name= $("#name").val();		
+	var url = script_url+"?callback=ctrlq&name="+name+"&id="+id1+"&action=update";
+	var request = jQuery.ajax({
+		crossDomain: true,
+		url: url ,
+		method: "GET",
+		dataType: "json"
+	});		
+}
 
-           $("#formRetrieve").submit(function(event){
-       // abort any pending request
-       if (request) {
-           request.abort();
-       }
-       // setup some local variables
-       var $form = $(this);
-       // let's select and cache all the fields
-       var $inputs = $form.find("input, select, button, textarea");
-       // serialize the data in the form
-       var serializedData = $form.serialize();
- 
-       // let's disable the inputs for the duration of the ajax request
-       // Note: we disable elements AFTER the form data has been serialized.
-       // Disabled form elements will not be serialized.
-       $inputs.prop("disabled", true);
-       resetForm();
-       $('#messageRetrieve').text('Requesting data...');
-                $('#datatable').text("");
+function delete_value(){
+	$("#re").css("visibility","hidden");
+	document.getElementById("loader").style.visibility = "visible";
+	$('#mySpinner').addClass('spinner');
+	var id1=	$("#id").val();
+	var name= $("#name").val();
+	var url = script_url+"?callback=ctrlq&name="+name+"&id="+id1+"&action=delete";
+	var request = jQuery.ajax({
+		crossDomain: true,
+		url: url ,
+		method: "GET",
+		dataType: "json"
+	});
+}
 
- 
-       // fire off the request to /form.php
-       request = $.ajax({
-           url: "https://script.google.com/macros/s/AKfycbxK6OlTh5D_HSAkgoQl9CgnmRmXsGbzpqzTqiC0isiFhJl0yeKN/exec",
-           type: "post",
-           data: serializedData
-       });
- 
-       // callback handler that will be called on success
-       request.done(function (response, textStatus, jqXHR){
-           // log a message to the console
-           $('#result').html('<a href="https://docs.google.com/spreadsheets/d/1KZ3jW3X7xtCzTP_iGhdsIUKjgHjzKqkvdXNn2z7izlA/edit?usp=sharing" target="_blank">Success - see Google Sheet</a>');
-     console.log("Retrieved.");
-     //console.log(response);
-     //console.log(textStatus);
-     //console.log(jqXHR);
-     var data = response.values;
-     //console.log(data);    
+// print the returned data
+function ctrlq(e) {		
+	$("#re").html(e.result);
+	$("#re").css("visibility","visible");
+	read_value();		
+}
 
-                  var trHTML ="";
-
-   $.each(data, function (i, item) {
-     var tdHTML="";   
-     //prepare first two columns
-     if (i==0){
-       tdHTML= "<td>Delete</td><td>Edit</td>" + tdHTML;
-       tdHTML=tdHTML.replace(/td>/gi,"th>");
-     }else{
-       tdHTML+= "<td><button onclick='recordDelete(this)' >delete</button></td>";
-       tdHTML+= "<td><button onclick='recordEdit(this)' >edit</button></td>";
-     }
-     //prepare remaining columns
-     for (j = 0; j < item.length; j++) {
-       tdHTML += "<td>" + item[j] + "</td>";
-     }
-     trHTML += "<tr>" + tdHTML + "</tr>";
-   }); //end each records
-
-   //append records to table element
-   $('#datatable').append(trHTML);
-   //reset
-   $('#messageRetrieve').text('');
-$('#messageRetrieve')[0].scrollIntoView(false);
-       });
- 
-       // callback handler that will be called on failure
-       request.fail(function (jqXHR, textStatus, errorThrown){
-           // log the error to the console
-           console.error(
-               "The following error occured: "+
-               textStatus, errorThrown
-           );
-       });
- 
-       // callback handler that will be called regardless
-       // if the request failed or succeeded
-       request.always(function () {
-           // reenable the inputs
-           $inputs.prop("disabled", false);
-       });
- 
-       // prevent default posting of form
-       event.preventDefault();
-   });
-
-           $("#formUpdate").submit(function(event){
-       // abort any pending request
-       if (request) {
-           request.abort();
-       }
-       // setup some local variables
-       var $form = $(this);
-       // let's select and cache all the fields
-       var $inputs = $form.find("input, select, button, textarea");
-       // serialize the data in the form
-       var serializedData = $form.serialize();
- 
-       // let's disable the inputs for the duration of the ajax request
-       // Note: we disable elements AFTER the form data has been serialized.
-       // Disabled form elements will not be serialized.
-       $inputs.prop("disabled", true);
-       resetForm();
-       $('#messageUpdate').text('Updating data...');
- 
-       // fire off the request to /form.php
-       request = $.ajax({
-           url: "https://script.google.com/macros/s/AKfycbxK6OlTh5D_HSAkgoQl9CgnmRmXsGbzpqzTqiC0isiFhJl0yeKN/exec",
-           type: "post",
-           data: serializedData
-       });
- 
-       // callback handler that will be called on success
-       request.done(function (response, textStatus, jqXHR){
-           // log a message to the console
-           $('#result').html('<a href="https://docs.google.com/spreadsheets/d/1KZ3jW3X7xtCzTP_iGhdsIUKjgHjzKqkvdXNn2z7izlA/edit?usp=sharing" target="_blank">Success - see Google Sheet</a>');
-     console.log("Updated.");
-     //console.log(response);
-     //console.log(textStatus);
-     //console.log(jqXHR);
-     resetForm();
-     $('#messageUpdate').text('');
-     $("#btnRetrieve").click();
-       });
- 
-       // callback handler that will be called on failure
-       request.fail(function (jqXHR, textStatus, errorThrown){
-           // log the error to the console
-           console.error(
-               "The following error occured: "+
-               textStatus, errorThrown
-           );
-       });
- 
-       // callback handler that will be called regardless
-       // if the request failed or succeeded
-       request.always(function () {
-           // reenable the inputs
-           $inputs.prop("disabled", false);
-       });
- 
-       // prevent default posting of form
-       event.preventDefault();
-   });
-
-
-           $("#formDelete").submit(function(event){
-       // abort any pending request
-       if (request) {
-           request.abort();
-       }
-       // setup some local variables
-       var $form = $(this);
-       // let's select and cache all the fields
-       var $inputs = $form.find("input, select, button, textarea");
-       // serialize the data in the form
-       var serializedData = $form.serialize();
- 
-       // let's disable the inputs for the duration of the ajax request
-       // Note: we disable elements AFTER the form data has been serialized.
-       // Disabled form elements will not be serialized.
-       $inputs.prop("disabled", true);
-       resetForm();
-       $('#messageDelete').text('Deleting data...');
- 
-       // fire off the request to /form.php
-       request = $.ajax({
-           url: "https://script.google.com/macros/s/AKfycbxK6OlTh5D_HSAkgoQl9CgnmRmXsGbzpqzTqiC0isiFhJl0yeKN/exec",
-           type: "post",
-           data: serializedData
-       });
- 
-       // callback handler that will be called on success
-       request.done(function (response, textStatus, jqXHR){
-           // log a message to the console
-           $('#result').html('<a href="https://docs.google.com/spreadsheets/d/1KZ3jW3X7xtCzTP_iGhdsIUKjgHjzKqkvdXNn2z7izlA/edit?usp=sharing" target="_blank">Success - see Google Sheet</a>');
-     console.log("Deleted.");
-     console.log(response);
-     console.log(textStatus);
-     console.log(jqXHR);
-     resetForm();
-     $('#messageDelete').text('');
-     $("#btnRetrieve").click();
-       });
- 
-       // callback handler that will be called on failure
-       request.fail(function (jqXHR, textStatus, errorThrown){
-           // log the error to the console
-           console.error(
-               "The following error occured: "+
-               textStatus, errorThrown
-           );
-       });
- 
-       // callback handler that will be called regardless
-       // if the request failed or succeeded
-       request.always(function () {
-           // reenable the inputs
-           $inputs.prop("disabled", false);
-       });
- 
-       // prevent default posting of form
-       event.preventDefault();
-   });
-
-
- });
-
-function recordEdit(param) {
-resetForm();
-var arrRecord=[];
-$(param).closest('tr').find('td').not(':first').each(function() {
-       //var textval = $(this).text();
-      arrRecord.push( $(this).text());
-  });
- $("#formUpdate #tid").val(arrRecord[1]);
- $("#formUpdate #name").val(arrRecord[3]);
- $("#formUpdate #comment").val(arrRecord[4]);
- //window.location.hash = '#formUpdate';
-       // var formUpdate = document.getElementById ("formUpdate");
-       //formUpdate.scrollIntoView(true);
-       $('#formUpdate')[0].scrollIntoView();
-   //alert(address.join('\n'));
-};
-
-function recordDelete(param) {
-resetForm();
-var arrRecord=[];
-$(param).closest('tr').find('td').not(':first').each(function() {
-       //var textval = $(this).text();
-      arrRecord.push( $(this).text());
-  });
- $("#formDelete #dtid").val(arrRecord[1]);
- $("#formDelete #dname").val(arrRecord[3]);
- //window.location.hash = '#formUpdate';
-       // var formUpdate = document.getElementById ("formUpdate");
-       //formUpdate.scrollIntoView(true);
-       $('#formDelete')[0].scrollIntoView();
-   //alert(address.join('\n'));
-};
-
-function resetForm(){
-   $('#formCreate')[0].reset();
-   $('#formUpdate')[0].reset();
-   $("#formUpdate tid").prop("disabled", true);
-   $('#formDelete')[0].reset();
-   $("#formDelete dtid").prop("disabled", true);     $("#formUpdate dname").prop("disabled", true);
+function read_value() {
+	$("#re").css("visibility","hidden");		   
+	document.getElementById("loader").style.visibility = "visible";
+	var url = script_url+"?action=read";
+	$.getJSON(url, function (json) {
+		// Set the variables from the results array
+		// CREATE DYNAMIC TABLE.
+		var table = document.createElement("table");
+		var header = table.createTHead();
+		var row = header.insertRow(0);     
+		var cell1 = row.insertCell(0);
+		var cell2 = row.insertCell(1);		
+		cell1.innerHTML = "<b>ID</b>";
+		cell2.innerHTML = "<b>Name</b>";			
+		// ADD JSON DATA TO THE TABLE AS ROWS.
+		for (var i = 0; i < json.records.length; i++) {
+			tr = table.insertRow(-1);
+			var tabCell = tr.insertCell(-1);
+			tabCell.innerHTML = json.records[i].ID;
+			tabCell = tr.insertCell(-1);
+			tabCell.innerHTML = json.records[i].NAME;
+		}
+		// FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+		var divContainer = document.getElementById("showData");
+		divContainer.innerHTML = "";
+		divContainer.appendChild(table);
+		document.getElementById("loader").style.visibility = "hidden";
+		$("#re").css("visibility","visible");
+	});
 }
